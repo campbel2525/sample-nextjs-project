c4418e8bec00c95f6a78bc09d781cf7b53794828
古谷竜一 <campbel@campbel.local>
2025-07-19 19:50:29 +0900

    Commit at 2025-07-19 19:50:29

--- infrastructures/terraform/src/aws/modules/_create_amplify/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "amplify_app_id" {
-  value       = aws_amplify_app.amplify.id
-  description = "The ID of the Amplify app."
-}
--- infrastructures/terraform/src/aws/project/apprunner_app/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "created_ecr_repository_name" {
-  description = "The name of the ECR repository created by the module."
-  value       = module.user_front_apprunner.ecr_repository_name
-}
-output "app_runner_url" {
-  description = "The URL of the App Runner service."
-  value       = module.user_front_apprunner.apprunner_service_url
-}
